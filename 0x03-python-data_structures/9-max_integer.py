6c7e51d701c62070c27976e3121bdf1907a7f921
MundiaNderi <sarahngima77@gmail.com>
2023-01-10 12:29:02 +0300

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-max_integer = __import__{'9-max_integer').max_integer
-
-my_list = [1, 90, 2, 13, 34, 4, -13, 3]
-max_value = max_integer(my_list)
-print("Max: {}".format(max_value))
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    my_list.sort()
+    return my_list[-1]
