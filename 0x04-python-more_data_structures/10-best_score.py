3d8bf19436c0989c2e8d1bc12ba92f7d0e468d24
MundiaNderi <sarahngima77@gmail.com>
2023-01-12 21:26:08 +0300

    Fixing Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best_score = max(a_dictionary, key=a_dictionary.get)
-    print(best_score)
+    if best_ score = 0:
+        return None
+    else:
+        best_score = max(a_dictionary, key=a_dictionary.get)
+        print(best_score)
