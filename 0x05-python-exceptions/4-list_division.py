0e7fad55aad6756c3edb3b24be420fc74680de36
MundiaNderi <sarahngima77@gmail.com>
2023-01-25 11:54:41 +0300

    Integers divsion with debug

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    for i in range(list_length):
+    for i in range(0, list_length):
         try:
             if i >= len(my_list_1) or i >= len(my_list_2):
                 raise IndexError
@@ -22,5 +22,5 @@ def list_division(my_list_1, my_list_2, list_length):
                 result.append(0)
             except ZeroDivisionError:
                 print("division by 0")
-                result.append(0)
+                result.appendi(0)
     return result
