5f482710c69db0b61a146fdc11d3661e73792453
MundiaNderi <sarahngima77@gmail.com>
2023-02-05 21:32:38 +0300

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,26 +2,13 @@
 
 class Square:
     """
-    Define a square by private instance attribute: size
-    Instantiation with size (no type/value verification)
+    Represents a square
     """
     def __init__(self, size):
         """
         Initialize square with given size
+        Args:
+            size: size of the square
         """
+
         self.__size = size
-
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
