c722ccd6987e8045bbe2772829d3c828fc60db2a
MundiaNderi <sarahngima77@gmail.com>
2023-02-12 12:08:27 +0300

    rectangle and main files

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module holds class Base"""
+import json
+
+
+class Base:
+    """Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes attributes for class Base
+        Args:
+            id: public instance attribute
+        """
+
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
