756eca76d7e10a9e85524059f545e574070ec96a
MundiaNderi <sarahngima77@gmail.com>
2023-04-15 09:59:00 +0300

    More rectangles

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const sq = require('./5-square');
+class Square extends sq {
+    charPrint (c) {
+        if (!c) {
+            this.print();
+        } else {
+            for (let i = 0; i < this.width; i++) {
+                console.log(c.repeat(this.width));
+            }
+        }
+    }
+}
