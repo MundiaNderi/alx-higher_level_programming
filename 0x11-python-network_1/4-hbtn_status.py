e0ae949b7e08bfa7347205371f56b6acecea3bcf
MundiaNderi <sarahngima77@gmail.com>
2023-04-29 09:07:37 +0300

    Status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-# Module that fetches a website
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    # fetches a specific website
-    url = 'https://intranet.hbtn.io/status'
-    r = requests.get(url)
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
