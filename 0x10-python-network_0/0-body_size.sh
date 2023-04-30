f4e3b87494d7a5fe0eac3bd88f69f836de50b47a
MundiaNderi <sarahngima77@gmail.com>
2023-04-30 23:20:20 +0300

    adding documentation to task 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays body size of URL
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 size=$(curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r\n')
