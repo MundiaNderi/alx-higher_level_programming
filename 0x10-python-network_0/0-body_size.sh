12b8ae190966f7e3e85a25a2320f63b073a9e04e
MundiaNderi <sarahngima77@gmail.com>
2023-04-30 23:25:25 +0300

    Changing shebang and script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-size=$(curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r\n')
+curl -sI "$1" | grep "Content-Length" | cut -d ':' -f 2 | cut -d ' ' -f 2
