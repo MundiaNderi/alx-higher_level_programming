f8bd0a5ffde186bc758da650a6ba0813728b469f
MundiaNderi <sarahngima77@gmail.com>
2023-05-23 19:16:44 +0300

    Fixing task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+
+// Check if the filename is provided as a command line argument
+if (process.argv.length < 3) {
+  console.log('Usage: node <script-name> <filename>');
+  process.exit(1);
+}
+
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', function (err, data) {
   if (!err) {
     console.log(data);
   } else {
-    console.log(err);
+    console.log('{ Error: ' + err.code + ': ' + err.path + '\n    at Error (native) }');
   }
 });
